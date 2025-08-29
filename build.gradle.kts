1d93de576e6cca907b4faf16595d8e3825152e17
GNRL-dev <ghost.mail@tuta.io>
2025-08-29 09:05:50 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,8 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.3.0")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:8d5a95efcd")
+       // classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
