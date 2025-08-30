68a0bcb7f1d1fd559a9c9c88e1f88dcc48f30c2a
GNRL-dev <ghost.mail@tuta.io>
2025-08-30 18:09:47 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension 
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import com.android.build.gradle.BaseExtension
 
 buildscript {
@@ -11,7 +11,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.4.0")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0") // Adjusted version to avoid metadata version conflicts
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
 
@@ -23,9 +23,12 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+// Extension functions for cleaner DSL
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
+    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
-fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
+fun Project.android(configuration: BaseExtension.() -> Unit) =
+    extensions.getByName<BaseExtension>("android").configuration()
 
 subprojects {
     apply(plugin = "com.android.library")
@@ -41,26 +44,22 @@ subprojects {
 
         defaultConfig {
             minSdk = 21
-            targetSdk = 34}
-        lint {
-            abortOnError = false}  
-    }
-        
+            targetSdk = 34
+        }
 
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_17
             targetCompatibility = JavaVersion.VERSION_17
         }
 
-        // Set Kotlin compiler options
         tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
             kotlinOptions {
-                jvmTarget = "17" // Required for Kotlin 1.9.x
+                jvmTarget = "17"
                 freeCompilerArgs = listOf(
-                    "-Xno-call-assertions", 
-                    "-Xno-param-assertions", 
+                    "-Xno-call-assertions",
+                    "-Xno-param-assertions",
                     "-Xno-receiver-assertions",
-                    "-Xskip-metadata-version-check"  // Skip metadata version check
+                    "-Xskip-metadata-version-check"
                 )
             }
         }
@@ -70,20 +69,17 @@ subprojects {
         val apk by configurations
         val implementation by configurations
 
-        // Cloudstream classes stub
         apk("com.lagradost:cloudstream3:pre-release")
-
-        // Optional dependencies
-        implementation ("com.github.recloudstream.cloudstream:library:v4.5.2")
+        implementation("com.github.recloudstream.cloudstream:library:v4.5.2")
         implementation(kotlin("stdlib", version = "1.9.0"))
-       // implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.13") // HTTP library
-        implementation("org.jsoup:jsoup:1.21.2") // HTML parser
+        implementation("com.github.Blatzar:NiceHttp:0.4.13")
+        implementation("org.jsoup:jsoup:1.21.2")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.+")
-        implementation("org.mozilla:rhino:1.7.14") // Rhino JS engine
+        implementation("org.mozilla:rhino:1.7.14")
     }
 }
 
+// Clean task at root level
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
