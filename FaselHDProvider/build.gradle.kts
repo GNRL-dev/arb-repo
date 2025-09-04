f05ef30f856a67ee8a22af90ad871bfe3b26263c
GNRL-dev <ghost.mail@tuta.io>
2025-09-04 17:57:36 +0100

    Update build.gradle.kts

--- FaselHDProvider/build.gradle.kts
+++ FaselHDProvider/build.gradle.kts
@@ -1,15 +1,39 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+cloudstream {
+    language = "ar"
+    //author = "YourName"
+    description = "Arabic streaming plugin for Cloudstream"
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    pluginVersionCode = 4.5
+  //  id = "faselhd.provider"
+}
+
 android {
     namespace = "com.faselhd"
-
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 34
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+}
+
+dependencies {
+    implementation("com.github.recloudstream.cloudstream:library:v4.5.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
+    implementation("com.github.Blatzar:NiceHttp:0.4.13")
+    implementation("org.jsoup:jsoup:1.21.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.+")
+    implementation("org.mozilla:rhino:1.7.14")
+    apk("com.lagradost:cloudstream3:pre-release")
 }
