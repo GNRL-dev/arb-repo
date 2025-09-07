e2482981a784f5063bde114928dd53e84f69e08b
GNRL-dev <ghost.mail@tuta.io>
2025-09-07 17:36:38 +0100

    Update build.gradle.kts

--- FaselHDProvider/build.gradle.kts
+++ FaselHDProvider/build.gradle.kts
@@ -1,11 +1,9 @@
-import org.gradle.api.JavaVersion
-
-/* plugins {
+ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("com.lagradost.cloudstream3.gradle")
-}*/
-//version = 1
+}
+version = 1
 
 cloudstream {
     language = "ar"
@@ -13,12 +11,12 @@ cloudstream {
     description = "Arabic streaming plugin for Cloudstream"
     status = 1
     tvTypes = listOf("Movie", "TvSeries")
-  //  id = "faselhd.provider"
+    //id = ("faselhd.provider")
 }
 
-  android {
+   android {
     namespace = "com.faselhd"
-    compileSdk = 31
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 21
@@ -29,13 +27,16 @@ cloudstream {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
-/*
-dependencies {
+ dependencies {
+    val apk by configurations
+
+    apk("com.lagradost:cloudstream3:pre-release")
+
     implementation("com.github.recloudstream.cloudstream:library:v4.5.2")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.10")
     implementation("com.github.Blatzar:NiceHttp:0.4.13")
     implementation("org.jsoup:jsoup:1.21.2")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.+")
-    implementation("org.mozilla:rhino:1.7.14")
-    apk("com.lagradost:cloudstream3:pre-release")
-}*/
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.0")
+    implementation("org.mozilla:rhino:1.8.0")
+}
+
