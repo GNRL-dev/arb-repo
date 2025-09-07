2744c98cde61eac9856de3827881afbbe3c9feff
GNRL-dev <ghost.mail@tuta.io>
2025-09-07 21:58:54 +0100

    Update build.gradle.kts

--- AkwamProvider/build.gradle.kts
+++ AkwamProvider/build.gradle.kts
@@ -1,13 +1,42 @@
-version = 2
+ plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+version = 1
 
 cloudstream {
-    authors = listOf( "Blatzar" )
-	
-	language = "ar"
-	
+    language = "ar"
+    author = "Gnrl"
+    description = "Arabic streaming plugin for Cloudstream"
     status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    //id = ("faselhd.provider")
+   // apiVersion = 4
+}
 
-    tvTypes = listOf( "TvSeries" , "Movie" , "Anime" , "Cartoon" )
+   android {
+    namespace = "com.akwam"
+    compileSdk = 35
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=akwam.to&sz=%size%"
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+ dependencies {
+    val apk by configurations
+
+    apk("com.lagradost:cloudstream3:pre-release")
+
+    implementation("com.github.recloudstream.cloudstream:library:v4.5.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
+    implementation("com.github.Blatzar:NiceHttp:0.4.13")
+    implementation("org.jsoup:jsoup:1.21.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.0")
+    implementation("org.mozilla:rhino:1.8.0")
 }
