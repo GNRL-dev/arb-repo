50c9c749105bea4fb5324f0d5cb0f809740fb054
GNRL-dev <ghost.mail@tuta.io>
2025-09-07 17:36:02 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,85 +1,42 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import com.android.build.gradle.BaseExtension
-//import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+ plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+version = 1
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10") 
-    }
+cloudstream {
+    language = "ar"
+    //author = "YourName"
+    description = "Arabic streaming plugin for Cloudstream"
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    //id = ("faselhd.provider")
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
+   android {
+    namespace = "com.faselhd"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
+ dependencies {
+    val apk by configurations
 
-// Utility extension functions
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
-    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+    apk("com.lagradost:cloudstream3:pre-release")
 
-fun Project.android(configuration: BaseExtension.() -> Unit) =
-    extensions.getByName<BaseExtension>("android").configuration()
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
-    }
-
-    android {
-        compileSdkVersion(31)
-
-        defaultConfig {
-            minSdk = 21
-            targetSdk = 31
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-    }
-
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = "1.8"
-        freeCompilerArgs = listOf(
-            "-Xno-call-assertions",
-            "-Xno-param-assertions",
-            "-Xno-receiver-assertions",
-            "-Xskip-metadata-version-check"
-        )
-    }
+    implementation("com.github.recloudstream.cloudstream:library:v4.5.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.10")
+    implementation("com.github.Blatzar:NiceHttp:0.4.13")
+    implementation("org.jsoup:jsoup:1.21.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.0")
+    implementation("org.mozilla:rhino:1.8.0")
 }
 
-
-    dependencies {
-        add("implementation", "com.github.recloudstream.cloudstream:library:v4.5.2")
-        add("implementation", "org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
-        add("implementation", "com.github.Blatzar:NiceHttp:0.4.13")
-        add("implementation", "org.jsoup:jsoup:1.21.2")
-        add("implementation", "com.fasterxml.jackson.module:jackson-module-kotlin:2.13.+")
-        add("implementation", "org.mozilla:rhino:1.7.14")
-        add("apk", "com.lagradost:cloudstream3:pre-release")
-    }
-}
-
-// Clean task
-tasks.register<Delete>("clean") {
-    delete(layout.buildDirectory)
-}
