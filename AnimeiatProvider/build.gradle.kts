02de168024a56b192564a3b068e694690d821020
GNRL-dev <ghost.mail@tuta.io>
2025-09-11 16:38:11 +0100

    Update build.gradle.kts

--- AnimeiatProvider/build.gradle.kts
+++ AnimeiatProvider/build.gradle.kts
@@ -1,13 +1,39 @@
-version = 2
+ plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+version = 1
 
 cloudstream {
-    authors = listOf( "ImZaw" )
-
-	language = "ar"
-	
+    language = "ar"
+    //author = "YourName"
+    description = "Arabic streaming plugin for Cloudstream"
     status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+  //  id = "faselhd.provider"
+}
 
-    tvTypes = listOf( "Anime" )
+   android {
+    namespace = "com.animeiat"
+    compileSdk = 35
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=animeiat.tv&sz=%size%"
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation("com.github.recloudstream.cloudstream:library:v4.5.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
+    implementation("com.github.Blatzar:NiceHttp:0.4.13")
+    implementation("org.jsoup:jsoup:1.21.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.+")
+    implementation("org.mozilla:rhino:1.7.14")
+    apk("com.lagradost:cloudstream3:pre-release")
 }
