224b561a3a8f9fbd430b4d88c364542ace841de7
GNRL-dev <ghost.mail@tuta.io>
2025-09-14 11:42:53 +0100

    Update FaselHDProvider.kt

--- FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDProvider.kt
+++ FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDProvider.kt
@@ -1,7 +1,7 @@
 package com.faselhd
 
 import com.lagradost.cloudstream3.*
-//import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.nicehttp.requestCreator
 import com.lagradost.cloudstream3.network.WebViewResolver
 import com.lagradost.cloudstream3.utils.M3u8Helper
